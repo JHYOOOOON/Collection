91e41e1cb7a2d30f973f47056ac743c6fc0e245b
Jeonghyun Yoon <50460114+YOONJHKD@users.noreply.github.com>
2019-07-16 18:46:58 +0900

    Create IronStick.cpp

---
+++ programmers/stack&queue/IronStick.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(string a) {
+    int answer = 0;
+    stack<char> s;
+
+    for(int i=0 ; i<a.length();i++){
+        if(a[i] =='(')
+            s.push(a[i]);
+        else{
+            s.pop();
+            if(a[i-1] =='(')
+                answer += s.size();
+            else
+                answer ++;
+        }
+    }
+    return answer;
+}
