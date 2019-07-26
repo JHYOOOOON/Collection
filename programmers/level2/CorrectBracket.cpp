bbd05ba7e96f94d2247606b3896b2261193fd627
Jeonghyun Yoon <50460114+YOONJHKD@users.noreply.github.com>
2019-07-26 22:27:35 +0900

    Create CorrectBracket.cpp

---
+++ programmers/level2/CorrectBracket.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char>arr;
+
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='('){
+            arr.push('(');
+        }
+        else{
+            if(arr.empty()){
+                return false;
+            }
+            else{
+                arr.pop();
+            }
+        }
+    }
+
+    return arr.empty()?true:false;
+}
