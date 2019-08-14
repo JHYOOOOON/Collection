335d065cbb8f9649f79a1a4e8975c9d0edbc3164
Jeonghyun Yoon <50460114+YOONJHKD@users.noreply.github.com>
2019-08-15 00:19:13 +0900

    Update and rename 체육복(continue).cpp to 체육복.cpp

--- programmers/level1/체육복.cpp
+++ programmers/level1/체육복.cpp
@@ -5,32 +5,30 @@ using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
     int answer = 0;
+    int temp=0;
     vector<int>ath;
+
     for(int i=0;i<n;i++){
         ath.push_back(1);
     }
     for(int i=0;i<lost.size();i++){
-        ath[lost[i]]--;
+        ath[lost[i]-1]--;
     }
     for(int i=0;i<reserve.size();i++){
-        ath[reserve[i]]++;
+        ath[reserve[i]-1]++;
     }
-    
+
     for(int i=0;i<n;i++){
-        if(ath[i]==2){
-            if(ath[i-1]==0){
-                ath[i-1]=1;
-                ath[i]=1;
+        if(ath[i]==1){answer++;}
+        else if(ath[i]==2){
+            answer++;
+            if(ath[i-1]==0&&i>0){
+                answer++;
             }
             else if(ath[i+1]==0){
                 ath[i+1]=1;
-                ath[i]=1;
             }
         }
-    }
-    
-    for(int i=0;i<n;i++){
-        if(ath[i]==1||ath[i]==2){answer++;}
     }
     return answer;
 }
