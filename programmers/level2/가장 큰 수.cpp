3fbfcddcd62c85205539eff22fa5fa04d8e1401e
Jeonghyun Yoon <50460114+YOONJHKD@users.noreply.github.com>
2019-08-16 03:36:32 +0900

    Create 가장 큰 수.cpp

---
+++ programmers/level2/가장 큰 수.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(int a, int b) {
+    string x = to_string(a);
+    string y = to_string(b);
+
+    return ( x + y > y + x) ? true : false;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+
+    sort(numbers.begin(), numbers.end(), cmp);
+
+    for(int i = 0 ; i < numbers.size() ; i++){
+        answer+=to_string(numbers[i]);
+    }
+
+    if(answer[0] == '0'){
+        return "0";
+    }
+
+    return answer;
+}
