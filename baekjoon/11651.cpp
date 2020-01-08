12531d716ba909f2476eb12983a960f01cb3b924
Jeonghyun Yoon <50460114+JHYOOOOON@users.noreply.github.com>
2020-01-08 20:50:36 +0900

    Create 11651.cpp

---
+++ baekjoon/11651.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool compare(pair<int, int> p1, pair<int, int> p2){
+    if(p1.second != p2.second){
+        return p1.second < p2.second;
+    }
+    return p1.first < p2.first;
+}
+
+int main(){
+    int n, x, y;
+    scanf("%d", &n);
+    vector<pair<int,int>> v;
+    for(int i=0; i<n; i++){
+        scanf("%d %d", &x, &y);
+        v.push_back({x, y});
+    }
+    sort(v.begin(), v.end(), compare);
+    for(int i=0; i<n; i++){
+        printf("%d %d\n", v[i].first, v[i].second);
+    }
+
+    return 0;
+}
