e8830a0adb79591302c3828d128d83cb8347595a
Jeonghyun Yoon <50460114+JHYOOOOON@users.noreply.github.com>
2020-01-18 23:56:32 +0900

    Create 11931.cpp

---
+++ baekjoon/11931.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    vector<int> v(N);
+    for(int i=0; i<N; i++) cin >> v[i];
+    sort(v.begin(), v.end(), greater<int>());
+    for(int i=0; i<N; i++){ cout << v[i] << "\n"; }
+    return 0;
+}
