e581bb789028aaa2ea94c4574b334abbecdbe759
Jeonghyun Yoon <jhyooooooon@gmail.com>
2020-01-30 01:10:58 +0900

    Create 10039.cpp

---
+++ baekjoon/10039.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[5], sum = 0;
+    for(int i=0; i<5; i++){
+        cin >> arr[i];
+        if(arr[i] < 40) arr[i] = 40;
+        sum += arr[i];
+    }
+    cout << sum/5 << "\n";
+
+    return 0;
+}
