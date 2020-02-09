389bfbdb7aa5695fc172b84a147503545b6bbbdb
Jeonghyun Yoon <jhyooooooon@gmail.com>
2020-02-10 01:15:06 +0900

    Create 4153.cpp

---
+++ baekjoon/4153.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[3];
+    while(1){
+        cin >> a[0] >> a[1] >> a[2];
+        if(!a[0] && !a[1] && !a[2]) break;
+        sort(a, a+3);
+        (a[0]*a[0] + a[1]*a[1] == a[2]*a[2]) ? cout << "right" << "\n" : cout << "wrong" << "\n";
+    }
+    return 0;
+}
