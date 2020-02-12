ebb1a1643ed633d393fdac708f4fcd16c8064254
Jeonghyun Yoon <jhyooooooon@gmail.com>
2020-02-13 02:17:05 +0900

    Create 2163.cpp

---
+++ baekjoon/2163.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool arr[101][101];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    cout << n * m - 1 << "\n";
+
+    return 0;
+}
