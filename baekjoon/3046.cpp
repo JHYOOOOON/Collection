9f8a435ddb47498a7d200c1eaf3bc6bc28b557c9
Jeonghyun Yoon <jhyooooooon@gmail.com>
2020-02-23 17:06:30 +0900

    Create 3046.cpp

---
+++ baekjoon/3046.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int r1, s;
+    cin >> r1 >> s;
+    cout << s * 2 - r1 << "\n";
+    return 0;
+}
