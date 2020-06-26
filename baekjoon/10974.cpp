d0a592bcb218551e3db01211d7e4cddbe2f33574
Jeonghyun Yoon <jhyooooooon@gmail.com>
2020-06-27 02:00:54 +0900

    Create 10974.cpp

---
+++ baekjoon/10974.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) v[i] = i + 1;
+    do {
+        for (int i = 0; i < n; i++) cout << v[i] << ' ';
+        cout << "\n";
+    } while (next_permutation(v.begin(), v.end()));
+    return 0;
+}
